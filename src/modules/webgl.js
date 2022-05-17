af393a0f31be396f8a61e4b3e6080b81ab69d46d
Adrián Gubrica <a_gubrica@Adrians-MacBook-Air.local>
2022-05-17 16:09:47 +0200

    updated gltf

--- src/modules/webgl.js
+++ src/modules/webgl.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three'
-import { GLTFLoader } from '../../node_modules/three/examples/jsm/loaders/GLTFLoader'
-import { DRACOLoader } from '../../node_modules/three/examples/jsm/loaders/DRACOLoader'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
 export default class WebGL {
   constructor(options) {
@@ -12,13 +13,16 @@ export default class WebGL {
     this.height = this.container.offsetHeight
 
     this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-    this.camera.position.z = 5
+    this.camera.position.z = 1
 
     this.renderer = new THREE.WebGLRenderer({
       alpha: true,
       outputEncoding: THREE.sRGBEncoding,
     })
+
     this.container.appendChild(this.renderer.domElement)
+
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement)
 
     this.time = 0
 
@@ -45,10 +49,10 @@ export default class WebGL {
     const loader = new GLTFLoader()
 
     loader.load(
-      'house_1',
+      'https://raw.githubusercontent.com/Gubr2/klauzura-ls-2122/574321a93e1140031124a4840324900b932934d2/src/gltf/house_1.gltf?raw=true',
       (gltf) => {
         console.log(gltf)
-        // this.scene.add(gltf.scene.children[0])
+        this.scene.add(gltf.scene.children[0])
       },
       undefined,
       function (error) {
